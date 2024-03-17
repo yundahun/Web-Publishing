bf32cf7dda7d3657ae3af7fb0761d7eeb7b4cbb3
yundahun <fkdlwldj02@naver.com>
2024-03-17 20:22:46 +0900

    2024.03.17 업로드

--- js/main.js
+++ js/main.js
@@ -6,12 +6,3 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
-
-new Swiper('.mySwiper', {
-    // Optional parameters
-    direction: 'horizontal', // 수평 슬라이드
-    loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
-    autoplay: { // 자동 재생 여부
-      delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
-    },
-});
