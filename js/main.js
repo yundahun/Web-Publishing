fd4102b58bacbca3caa532ffef5b67f598b92cbd
dahun <fkdlwldj02@naver.com>
2024-03-18 16:49:04 +0900

    2024.03.18 수업시간 업로드

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+new Swiper('.promotion .swiper', {
+    // Optional parameters
+    direction: 'horizontal', // 수평 슬라이드
+    loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
+    autoplay: { // 자동 재생 여부
+      delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
     },
 });
