aba8c83040d7b4ff9176c6579b747c0602565947
dahun <fkdlwldj02@naver.com>
2024-03-20 16:48:31 +0900

    2024.03.20 프로젝트 업로드

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ new Swiper('.promotion .swiper', {
       delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
     },
 });
+
+new Swiper('.main-coffee .d-coffee .swiper', {
+    // Optional parameters
+    direction: 'horizontal', // 수평 슬라이드
+    loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
+    autoplay: {
+      delay: 3000
+    },
+    slidesPerView: 5,
+    spaceBetween: 10,
+  centeredSlides: true,
+
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const burgerEl = document.querySelector('.btn-hamburger');
+const closeEl = document.querySelector('.btn-close')
+const navEl = document.querySelector('.m-nav')
+
+burgerEl.addEventListener('click', function () {
+  navEl.classList.add('active')
+})
+closeEl.addEventListener('click', function () {
+  navEl.classList.remove('active')
+})
